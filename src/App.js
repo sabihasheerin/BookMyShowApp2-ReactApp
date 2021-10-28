050fb60717cefc6dfaec214e61c2b7434de96407
Abdul Rahiman Shaik <sabihasheerin.s@gmail.com>
2021-10-28 08:29:05 +0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,46 @@
+import './App.css';
+import Header from './components/Header';
+import SliderImages from './components/SliderImages';
+import CardsSlider from './components/CardsSlider';
+import Premieres from './components/Premieres';
+import Footer from './components/Footer';
+// import AllMovies from './components/AllMovies';
+import AllMoviesFetch from './components/AllMoviesFetch';
+// import SinglMovie from './components/SingleMovie';
+import SingleMovieFetch from './components/SingleMovieFetch';
+import Registration from './components/Registration';
+
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+
+function App() {
+  return (
+    <Router>
+      <div>
+        <Header/>
+          <Switch>
+            <Route exact path="/">
+              <SliderImages/>
+              <CardsSlider/>
+              <Premieres/>
+            </Route>
+            <Route exact path="/movies">
+              <AllMoviesFetch />
+            </Route>
+            <Route exact path="/movie/:movid">
+              <SingleMovieFetch/>
+            </Route> 
+            <Route exact path="/register">
+              <Registration/>
+            </Route> 
+          </Switch>
+        <Footer/>
+      </div>
+    </Router>
+  );
+}
+export default App;
---
+++ src/components/rough.js
@@ -0,0 +1,85 @@
+import { Container } from "react-bootstrap";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Card from "react-bootstrap/Card";
+
+function AllMovies() {
+    return(
+        <div>
+            <Container fluid style ={{padding:"5%", background:"pink"}}>
+            <Row style={{textAlign:"center"}}>
+                    <Col  xs={6} md={4} lg={3} style={{marginBottom:"2%"}}>
+                    <Card>
+                        <Card.Img variant="top" src="./logo192.png" />
+                        <Card.Body>
+                        <Card.Title>Card title</Card.Title>
+                        <Card.Text>
+                            Bell Botttom Movie Name
+                        </Card.Text>
+                        </Card.Body>
+                    </Card>
+                    </Col>
+                    <Col  xs={6} md={4} lg={3} style={{marginBottom:"2%"}}>
+                    <Card>
+                        <Card.Img variant="top" src="./logo192.png" />
+                        <Card.Body>
+                        <Card.Title>Card title</Card.Title>
+                        <Card.Text>
+                        Bell Botttom Movie Name
+                        </Card.Text>
+                        </Card.Body>
+                    </Card>
+                    </Col>
+                    <Col  xs={6} md={4} lg={3} style={{marginBottom:"2%"}}>
+                    <Card>
+                        <Card.Img variant="top" src="./logo192.png" />
+                        <Card.Body>
+                        <Card.Title>Card title</Card.Title>
+                        <Card.Text>
+                        Bell Botttom Movie Name
+                        </Card.Text>
+                        </Card.Body>
+                    </Card>
+                    </Col>
+                    <Col  xs={6} md={4} lg={3} style={{marginBottom:"2%"}}>
+                    <Card>
+                        <Card.Img variant="top" src="./logo192.png" />
+                        <Card.Body>
+                        <Card.Title>Card title</Card.Title>
+                        <Card.Text>
+                        Bell Botttom Movie Name
+                        </Card.Text>
+                        </Card.Body>
+                    </Card>
+                    </Col>
+                    <Col  xs={6} md={4} lg={3} style={{marginBottom:"2%"}}>
+                    <Card>
+                        <Card.Img variant="top" src="./logo192.png" />
+                        <Card.Body>
+                        <Card.Title>Card title</Card.Title>
+                        <Card.Text>
+                        Bell Botttom Movie Name
+                        </Card.Text>
+                        </Card.Body>
+                    </Card>
+                    </Col>
+                    <Col  xs={6} md={4} lg={3} style={{marginBottom:"2%"}}>
+                    <Card>
+                        <Card.Img variant="top" src="./logo192.png" />
+                        <Card.Body>
+                        <Card.Title>Card title</Card.Title>
+                        <Card.Text>
+                        Bell Botttom Movie Name
+                        </Card.Text>
+                        </Card.Body>
+                    </Card>
+                    </Col>
+                    
+                </Row>
+            </Container>
+
+        </div>
+
+    );
+}
+export default AllMovies;
